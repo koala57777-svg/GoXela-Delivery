efd3e6df267c855c63a6d00daefcd42a5bf9fdc1
Gerber Ramírez <koala57777@gmail.com>
2026-08-30 19:21:07 -0600

    Cambio nombre de AyudanteConsola, y base ValidarTexto.

--- GoXelaDelivery/GoXelaDelivery/AyudanteConsola.cs
+++ GoXelaDelivery/GoXelaDelivery/AyudanteConsola.cs
@@ -3,18 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static GoXelaDelivery.Enums;
 using static System.Net.Mime.MediaTypeNames;
 
 namespace GoXelaDelivery
 {
     public static class AyudanteConsola
     {
+
         public static string ValidarTexto(string mensajeSolicitudDeDatos, int limiteCaracteres)
         {
             int anchoConsola = Console.WindowWidth;
             int espacios = (anchoConsola - mensajeSolicitudDeDatos.Length) / 2;
             if (espacios < 0) espacios = 0
-            Console.WriteLine($"{mensajeSolicitudDeDatos.}");
+            Console.WriteLine(new string(' ', espacios) + mensajeSolicitudDeDatos.ToUpper);
         } 
     }
+
 }
