0b2e8a4c915e4c295c81f0c57d6b4a032ca7bb69
Gerber Ramírez <koala57777@gmail.com>
2026-08-30 18:49:12 -0600

    Se agregó llave faltante en clase Enums.

--- GoXelaDelivery/GoXelaDelivery/AyudanteConsola.cs.cs
+++ GoXelaDelivery/GoXelaDelivery/AyudanteConsola.cs.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace GoXelaDelivery
 {
@@ -10,6 +11,9 @@ namespace GoXelaDelivery
     {
         public static string ValidarTexto(string mensajeSolicitudDeDatos, int limiteCaracteres)
         {
+            int anchoConsola = Console.WindowWidth;
+            int espacios = (anchoConsola - mensajeSolicitudDeDatos.Length) / 2;
+            if (espacios < 0) espacios = 0
             Console.WriteLine($"{mensajeSolicitudDeDatos.}");
         } 
     }
