cf325d429ee1e7ab9151a07e0713f500bfa37d18
Emanuel Ramírez <er6348100@gmail.com>
2026-08-31 09:48:28 -0600

    Implementa Clase Hija ProductoRefrigerado de Paquete, con constructor, y método Override CalcularCostoTipo.

--- GoXelaDelivery/GoXelaDelivery/Paquete.cs
+++ GoXelaDelivery/GoXelaDelivery/Paquete.cs
@@ -202,4 +202,17 @@ namespace GoXelaDelivery
             Console.WriteLine("Calcula el costo del tipo paquete PaqueteFragil...");
         }
     }
+
+	internal class ProductoRefrigerado : Paquete
+	{
+        public ProductoRefrigerado(string nuevoCodigoUnico, string nuevoPrefijo, TipoPaquete nuevoTipoPaquete, string nuevaDescripcion, double nuevoPeso, double nuevoValorDeclarado, Cliente nuevoClientePaquete, string nuevaDireccionOrigen, Cliente nuevaDireccionDestino, EstadoPaquete nuevoEstadoPaquete, Municipio nuevoMunicipioOrigen, Cliente nuevoMunicipioDestino) : base(nuevoCodigoUnico, nuevoPrefijo, nuevoTipoPaquete, nuevaDescripcion, nuevoPeso, nuevoValorDeclarado, nuevoClientePaquete, nuevaDireccionOrigen, nuevaDireccionDestino, nuevoEstadoPaquete, nuevoMunicipioOrigen, nuevoMunicipioDestino)
+        {
+
+        }
+
+        protected override void CalcularCostoTipo()
+        {
+            Console.WriteLine("Calcula el costo del tipo paquete ProductoRefrigerado...");
+        }
+    }
 }
