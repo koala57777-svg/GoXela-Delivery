8cc258b14804358ac6fa8bcd90cadb6c15a63f8d
Gerber Ramírez <koala57777@gmail.com>
2026-08-31 11:21:22 -0600

    Añade función ValidarTelefono a AyudanteConsola.

--- GoXelaDelivery/GoXelaDelivery/AyudanteConsola.cs
+++ GoXelaDelivery/GoXelaDelivery/AyudanteConsola.cs
@@ -98,6 +98,63 @@ namespace GoXelaDelivery
             } while (true);
             return numeroConvertido;
         }
+
+        public static int ValidarTelefono()
+        {
+            string mensajeSolicitudDeDatos = "Ingrese su número de teléfono";
+            int tamanoRequerido = 8;
+            int anchoConsola = Console.WindowWidth;
+            int espacios = (anchoConsola - mensajeSolicitudDeDatos.Length) / 2;
+            if (espacios < 0) espacios = 0;
+            Console.WriteLine(new string(' ', espacios) + mensajeSolicitudDeDatos.ToUpper());
+            Console.Write($"Presione Enter para enviar. \nEl número debe ser de 8 dígitos.\n\n Ingresar: ");
+            StringBuilder sb = new StringBuilder(tamanoRequerido, tamanoRequerido);
+            int numeroConvertido;
+            do
+            {
+                ConsoleKeyInfo teclaInfo = Console.ReadKey(intercept: true);
+                if (teclaInfo.Key == ConsoleKey.Enter && sb.Length == tamanoRequerido)
+                {
+                    if (int.TryParse(sb.ToString(), out numeroConvertido))
+                    {
+                        break;
+                    }
+                }
+                if (teclaInfo.Key == ConsoleKey.Backspace && sb.Length > 0)
+                {
+                    if (sb.Length == 5)
+                    {
+                        Console.Write("\b \b");
+                    }
+                    sb.Remove(sb.Length - 1, 1);
+                    Console.Write("\b \b");
+                }
+                if (sb.Length < tamanoRequerido)
+                {
+                    if (char.IsDigit(teclaInfo.KeyChar))
+                    {
+                        if (teclaInfo.KeyChar == '0')
+                        {
+                            if (sb.Length > 0)
+                            {
+                                sb.Append('0');
+                                Console.Write('0');
+                            }
+                        }
+                        else
+                        {
+                            sb.Append(teclaInfo.KeyChar);
+                            Console.Write(teclaInfo.KeyChar);
+                        }
+                        if (sb.Length == 4)
+                        {
+                            Console.Write("-");
+                        }
+                    }
+                }
+            } while (true);
+            return numeroConvertido;
+        }
     }
 
 }
--- PruebasCodigo/PruebasCodigo/Program.cs
+++ PruebasCodigo/PruebasCodigo/Program.cs
@@ -96,15 +96,70 @@ namespace PruebasCodigo
             } while (true);
             return numeroConvertido;
         }
+
+        public static int ValidarTelefono()
+        {
+            string mensajeSolicitudDeDatos = "Ingrese su número de teléfono";
+            int tamanoRequerido = 8;
+            int anchoConsola = Console.WindowWidth;
+            int espacios = (anchoConsola - mensajeSolicitudDeDatos.Length) / 2;
+            if (espacios < 0) espacios = 0;
+            Console.WriteLine(new string(' ', espacios) + mensajeSolicitudDeDatos.ToUpper());
+            Console.Write($"Presione Enter para enviar. \nEl número debe ser de 8 dígitos.\n\n Ingresar: ");
+            StringBuilder sb = new StringBuilder(tamanoRequerido, tamanoRequerido);
+            int numeroConvertido;
+            do
+            {
+                ConsoleKeyInfo teclaInfo = Console.ReadKey(intercept: true);
+                if (teclaInfo.Key == ConsoleKey.Enter && sb.Length == tamanoRequerido)
+                {
+                    if (int.TryParse(sb.ToString(), out numeroConvertido))
+                    {
+                        break;
+                    }
+                }
+                if (teclaInfo.Key == ConsoleKey.Backspace && sb.Length > 0)
+                {
+                    if (sb.Length == 5)
+                    {
+                        Console.Write("\b \b");
+                    }
+                    sb.Remove(sb.Length - 1, 1);
+                    Console.Write("\b \b");
+                }
+                if (sb.Length < tamanoRequerido)
+                {
+                    if (char.IsDigit(teclaInfo.KeyChar))
+                    {
+                        if (teclaInfo.KeyChar == '0')
+                        {
+                            if (sb.Length > 0)
+                            {
+                                sb.Append('0');
+                                Console.Write('0');
+                            }
+                        }
+                        else
+                        {
+                            sb.Append(teclaInfo.KeyChar);
+                            Console.Write(teclaInfo.KeyChar);
+                        }
+                        if (sb.Length == 4)
+                        {
+                            Console.Write("-");
+                        }
+                    }
+                }
+            } while (true);
+            return numeroConvertido;
+        }
     }
     internal class Program
     {
         static void Main(string[] args)
         {
-            string nombre = AyudanteConsola.ValidarTexto("Ingrese su nombre completo", 16);
-            Console.WriteLine($"\n\n{nombre}");
-            int codigo = AyudanteConsola.ValidarNumerico("Ingrese el número de código", 3);
-            Console.WriteLine($"\n\n{codigo}");
+            int numeroTelefono = AyudanteConsola.ValidarTelefono();
+            Console.WriteLine($"\n\n{numeroTelefono}");
         }
     }
 }
