aa92797f26d54acd9f84e6e0e68f4529b466ad20
Emanuel Ramírez <er6348100@gmail.com>
2026-08-31 18:49:45 -0600

    Se corrigió y quitó los instanciamientos innecesarios dentro de los métodos get y set de las listas dentro de la clase delivery, además se quitó las sublistas que anteriormente se debieron haber quitado.

--- GoXelaDelivery/GoXelaDelivery/Delivery.cs
+++ GoXelaDelivery/GoXelaDelivery/Delivery.cs
@@ -15,21 +15,7 @@ namespace GoXelaDelivery
 
 		private List<List<Vehiculo>> listasVehiculos = new List<List<Vehiculo>>();
 
-		private List<Bicicleta> listaBicicletas = new List<Bicicleta>();
-
-		private List<Motocicleta> listaMotocicletas = new List<Motocicleta>();
-
-		private List<Carro> listaCarros = new List<Carro>();
-
 		private List<List<Paquete>> listasPaquetes = new List<List<Paquete>>();
-
-		private List<Documento> listaDocumentos = new List<Documento>();
-
-		private List<PaqueteEstandar> listaPaquetesEstandar = new List<PaqueteEstandar>();
-
-		private List<PaqueteFragil> listaPaquetesFragiles = new List<PaqueteFragil>();
-
-		private List<ProductoRefrigerado> listaProductosRefrigerados = new List<ProductoRefrigerado>();
 
 		private List<Entrega> listaEntregas = new List<Entrega>();
 
@@ -179,32 +165,32 @@ namespace GoXelaDelivery
 
 		public List<Entrega> ListaEntregas
 		{
-			get { return listaEntregas = new List<Entrega>(); }
-			set { listaEntregas = new List<Entrega>() = value; }
+			get { return listaEntregas; }
+			set { listaEntregas = value; }
 		}
 
-		public List<List<Paquete>> ListaPaquetes
+		public List<List<Paquete>> ListasPaquetes
 		{
-			get { return listaPaquetes = new List<List<Paquete>>; }
-			set { listaPaquetes = new List<List<Paquete>> = value; }
+			get { return listasPaquetes; }
+			set { listasPaquetes = value; }
 		}
 
 		public List<List<Vehiculo>> ListaVehiculos
 		{
-			get { return listaVehiculos = new List<List<Vehiculo>>(); }
-			set { listaVehiculos = new List<List<Vehiculo>>() = value; }
+			get { return listasVehiculos; }
+			set { listasVehiculos = value; }
 		}
 
 		public List<Repartidor> ListaRepartidores
 		{
-			get { return listaRepartidores = new List<Repartidor>(); }
-			set { listaRepartidores = new List<Repartidor>() = value; }
+			get { return listaRepartidores; }
+			set { listaRepartidores =  value; }
 		}
 
 		public List<Cliente> ListaClientes
 		{
-			get { return listaClientes = new List<Cliente>(); }
-			set { listaClientes = new List<Cliente>() = value; }
+			get { return listaClientes; }
+			set { listaClientes = value; }
 		}
 	}
 }
