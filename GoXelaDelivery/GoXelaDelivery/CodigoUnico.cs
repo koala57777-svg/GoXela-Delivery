cc7c372c85a4f6c50556683fab8c57d59cffe589
Gerber Ramírez <koala57777@gmail.com>
2026-09-02 08:58:17 -0600

    Agrega función GenerarCodigoUnico en clase CodigoUnico.

--- GoXelaDelivery/GoXelaDelivery/CodigoUnico.cs
+++ GoXelaDelivery/GoXelaDelivery/CodigoUnico.cs
@@ -8,5 +8,43 @@ namespace GoXelaDelivery
 {
     internal class CodigoUnico
     {
+        
+        private HashSet<string> baseDeCodigosUnicos = new HashSet<string>();
+
+        
+        private Dictionary<string, int> ultimosNumeros = new Dictionary<string, int>();
+
+        
+        private static readonly object bloqueo = new object();
+
+        public string GenerarCodigoUnico(string prefijo)
+        {
+            lock (bloqueo)
+            {
+                if (!ultimosNumeros.TryGetValue(prefijo, out int numeroActual))
+                {
+                    numeroActual = 0;
+                }
+
+                string codigoCandidato;
+
+                do
+                {
+                    numeroActual++;
+                    codigoCandidato = $"{prefijo}-{numeroActual:D3}";
+                }
+                while (ExisteEnBaseDeDatos(codigoCandidato));
+
+                ultimosNumeros[prefijo] = numeroActual;
+                baseDeCodigosUnicos.Add(codigoCandidato);
+
+                return codigoCandidato;
+            }
+        }
+
+        private bool ExisteEnBaseDeDatos(string codigo)
+        {
+            return baseDeCodigosUnicos.Contains(codigo);
+        }
     }
 }
