7d52aaaa9b262112c40ede105e83a890a1e86550
Emanuel Ramírez <er6348100@gmail.com>
2026-09-05 16:24:30 -0600

    uso de Globales en incidente

--- GoXelaDelivery/GoXelaDelivery/Incidente.cs
+++ GoXelaDelivery/GoXelaDelivery/Incidente.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using static GoXelaDelivery.Enums;
-
+using static GoXelaDelivery.Globales;
 namespace GoXelaDelivery
 {
     internal class Incidente
