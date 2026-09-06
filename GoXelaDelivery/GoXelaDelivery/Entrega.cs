6cafccf341b48b6538a31ba963c52435a364e465
Emanuel Ramírez <er6348100@gmail.com>
2026-09-05 18:16:41 -0600

    Se quitó el parámetro al no ser necesario.

--- GoXelaDelivery/GoXelaDelivery/Entrega.cs
+++ GoXelaDelivery/GoXelaDelivery/Entrega.cs
@@ -50,7 +50,7 @@ namespace GoXelaDelivery
 
 		private double total;
 
-		public Entrega(Paquete nuevoPaqueteEntrega, TipoVehiculoGeneral nuevoVehiculoGeneral, double nuevaDistanciaEstimada, TipoServicio nuevoTipoServicio, double nuevaTarifaBase, Cliente clienteEntrega)
+		public Entrega(Paquete nuevoPaqueteEntrega, TipoVehiculoGeneral nuevoVehiculoGeneral, double nuevaDistanciaEstimada, TipoServicio nuevoTipoServicio, double nuevaTarifaBase)
 		{
 			EstadoEntrega = EstadoEntrega.Solicitado;
 			PaqueteEntrega = nuevoPaqueteEntrega;
@@ -59,7 +59,6 @@ namespace GoXelaDelivery
 			TipoServicio = nuevoTipoServicio;
 			TarifaBase = nuevaTarifaBase;
 			listaIncidentes = new List<Incidente>();
-			ClienteEntrega = clienteEntrega;
 		}
 
 		internal void AgregarIncidenteAEntrega(Entrega entregaAAgregarIncidente, Incidente incidenteAAgregar)
